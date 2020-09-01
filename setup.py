2422798fcdc5ca9aae204f93ae0c0fb72971b1cb
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2020-09-01 02:29:26 -0300

    Creating project and adding first dependencies

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sala_de_estudos_flask",
+    version="0.1.0",
+    description="Backend for saladeestudos.esy.es",
+    include_package_data=True
+)
