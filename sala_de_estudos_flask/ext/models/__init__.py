358d7bf6e30b61f8703521f0c7ac5eca0a3db7ef
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2020-09-01 03:01:53 -0300

    Creating evaluations model

--- sala_de_estudos_flask/ext/models/__init__.py
+++ sala_de_estudos_flask/ext/models/__init__.py
@@ -1,3 +1,4 @@
 from sala_de_estudos_flask.ext.models.professors import Professor
 from sala_de_estudos_flask.ext.models.subjects import Subject, ProfessorSubject, SubjectTypeEnum
 from sala_de_estudos_flask.ext.models.lessons import Lesson, ProfessorLesson
+from sala_de_estudos_flask.ext.models.evaluations import Evaluation, EvaluationKey, EvaluatioTypeEnum, PeriodEnum
