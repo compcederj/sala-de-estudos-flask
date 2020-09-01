cfb4557c525c3194af15c7e3016e70581fc4476b
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2020-09-01 02:43:19 -0300

    Migration of professors' model

--- sala_de_estudos_flask/ext/migrate.py
+++ sala_de_estudos_flask/ext/migrate.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from sala_de_estudos_flask.ext.db import db
+from sala_de_estudos_flask.ext.models import *  # noqa
 
 
 migrate = Migrate()
