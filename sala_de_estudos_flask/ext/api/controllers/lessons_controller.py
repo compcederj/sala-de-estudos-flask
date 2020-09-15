83245357cba61bf65f5c06ec4553a4098a76c5da
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2020-09-15 00:35:03 -0300

    [#3][fix] Returning received data and adding a check if there is data

--- sala_de_estudos_flask/ext/api/controllers/lessons_controller.py
+++ sala_de_estudos_flask/ext/api/controllers/lessons_controller.py
@@ -1,7 +1,20 @@
+from http.client import NOT_FOUND
+
+from sala_de_estudos_flask.ext.api.controllers import Controller
 from sala_de_estudos_flask.ext.dao.lessons_dao import LessonsDAO
 
 
-class LessonsController:
-    @staticmethod
-    def list(subject_id):
+class LessonsController(Controller):
+
+    def list(self, subject_id):
         lessons = LessonsDAO.list_lessons_by_subject(subject_id)
+
+        if lessons:
+            return self.as_json(lessons)
+
+        return self.make_error(
+            NOT_FOUND,
+            "Not Found",
+            "It was not found lessons for this subject. "
+            "Try another subject."
+        )
