a3c5cdb6b8285dc53f7a9d08d2c88394cbdee2ba
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2021-04-10 12:15:29 -0300

    [#16][feat] using connexion to create a flask app

--- sala_de_estudos_flask/app.py
+++ sala_de_estudos_flask/app.py
@@ -1,10 +1,15 @@
+import os.path
+
+from connexion import FlaskApp
 from flask import Flask, redirect
 
 from sala_de_estudos_flask.ext import config
 
 
 def create_app() -> Flask:
-    app = Flask(__name__)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    connexion_app = FlaskApp(__name__, specification_dir=basedir)
+    app = connexion_app.app
     config.init_app(app)
 
     @app.route("/")
