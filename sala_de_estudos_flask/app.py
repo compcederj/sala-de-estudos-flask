78de61cc685b050cf5766045a8378a18ee23532d
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2021-04-10 23:28:51 -0300

    [#16][feat] setting /api/v2 with connexion and swagger

--- sala_de_estudos_flask/app.py
+++ sala_de_estudos_flask/app.py
@@ -4,13 +4,17 @@ from connexion import FlaskApp
 from flask import Flask, redirect
 
 from sala_de_estudos_flask.ext import config
+from sala_de_estudos_flask.ext.api import add_apis
 
 
 def create_app() -> Flask:
     basedir = os.path.abspath(os.path.dirname(__file__))
     connexion_app = FlaskApp(__name__, specification_dir=basedir)
+
     app = connexion_app.app
     config.init_app(app)
+
+    add_apis(connexion_app)
 
     @app.route("/")
     def home():
